4a1f293b9dd53ab5bf211dcb73d44d41f84bb837
tomascambala <cambalatomas@gmail.com>
2021-04-23 15:58:58 +0200

    WIP

---
+++ ui/src/views/Clients.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Words</h1>
+    <table id="words" class="ui celled compact table">
+      <thead>
+        <tr>
+          <th>English</th>
+          <th>German</th>
+          <th colspan="3"></th>
+        </tr>
+      </thead>
+      <tr v-for="(word, i) in words" :key="i">
+        <!-- <td>{{ word.english }}</td>
+        <td>{{ word.german }}</td> -->
+        <!-- <td width="75" class="center aligned">
+          <router-link :to="{ name: 'show', params: { id: word._id }}">Show</router-link>
+        </td>
+        <td width="75" class="center aligned">
+          <router-link :to="{ name: 'edit', params: { id: word._id }}">Edit</router-link>
+        </td>
+        <td width="75" class="center aligned" @click.prevent="onDestroy(word._id)">
+          <a :href="`/words/${word._id}`">Destroy</a>
+        </td> -->
+      </tr>
+    </table>
+  </div>
+</template>
